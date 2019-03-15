6d020422b60d342c65b83d2add7c0d123db5bc81
Varchasvi Chethan <cherrychethan601@gmail.com>
2019-03-15 10:38:39 +0530

    sec comment

--- app/src/main/java/com/example/attendancemanager/userprofileactivity.java
+++ app/src/main/java/com/example/attendancemanager/userprofileactivity.java
@@ -1,23 +1,30 @@
 package com.example.attendancemanager;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class userprofileactivity extends AppCompatActivity {
 
     private EditText etRollNumber;
     private EditText etName;
     private Button btSave;
+    private SharedPreferences sharedPreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile2);
+
+        sharedPreferences = this.getSharedPreferences(
+                "attendance",MODE_PRIVATE
+        );
 
         etName = findViewById(R.id.name);
         etRollNumber = findViewById(R.id.roll_number);
@@ -28,7 +35,13 @@ public class userprofileactivity extends AppCompatActivity {
             public void onClick(View v) {
                 String name = etName.getText().toString();
                 String roll = etRollNumber.getText().toString();
-                Log.i("User","Name : " + name);
+                Log.i("User","Name : " + name + "Roll No: " + roll);
+                Toast.makeText(userprofileactivity.this, "Name :"+ name + "Roll No :"+roll+ ,
+                        Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor edit = sharedPreferences.edit();
+                edit.putString(s:"name",name);
+                edit.putString(s:"rollnumber",roll);
+                edit.apply();
             }
         });
 
